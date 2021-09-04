66737e2429f20dfd497a2c60a56936b8ff438cbc
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-04 18:15:21 +0530

    generate n terms of the fib series

--- Program.cs
+++ Program.cs
@@ -4,9 +4,27 @@ namespace LogicaPrograms
 {
     class Program
     {
+        public static void fibSeries(int terms)
+        {
+            int a = 0, b = 1, c = 0;
+            if (terms == 0)
+            {
+                Console.Write(a +" ");
+            }
+            for (int i = 2; i <= terms; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.Write(b +" ");
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logica Programs");
+            Console.WriteLine("Enter the num of terms of fibonacci series");
+            int terms = Convert.ToInt32(Console.ReadLine());
+            fibSeries(terms);
         }
     }
 }
